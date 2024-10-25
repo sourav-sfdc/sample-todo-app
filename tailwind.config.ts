c76c4720dbb4174c585e43ba8c00b5be13e29c63
sourav-developer <sourav.saha@platinumpmo.com>
2024-10-26 00:39:00 +0530

    next ui dependency added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
+import {nextui} from '@nextui-org/theme';
 import type { Config } from "tailwindcss";
 
 const config: Config = {
     darkMode: ["class"],
-    content: [
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
   	extend: {
@@ -58,7 +60,6 @@ const config: Config = {
   		},
   	}
   },
-	// eslint-disable-next-line @typescript-eslint/no-require-imports
-  plugins: [require("tailwindcss-animate")],
+  plugins: [nextui()],
 };
 export default config;
